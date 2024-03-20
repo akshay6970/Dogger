92ba8cdca8cfa9ab8e130315efc36f5ae4e0d562
akshay6970 <149579502+akshay6970@users.noreply.github.com>
2024-03-20 22:36:46 +0530

    second-day-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function openSidebar(){
+
+    let sidebar = document.querySelector('.nav-links');
+    sidebar.classList.add('openside-bar');
+    // alert("hello");
+}
+function closeSidebar(){
+
+    let sidebar = document.querySelector('.nav-links');
+    sidebar.classList.remove('openside-bar');
+    // alert("hello");
+}
